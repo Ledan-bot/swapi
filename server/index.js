b3b79b6e4ecd391617b2ebd8fee10d957e4c9dea
Nicholas Ledan <Nickledan5@gmail.com>
2021-11-13 16:24:47 -0500

    Configured Routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios').default;
 const bodyParser = require('body-parser');
+const { correctTitle, searchSWAPI } = require('./routes/search.js');
 
 
 const PORT = process.env.PORT || 8006;
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/api/search/:movie', correctTitle, searchSWAPI)
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
---
+++ server/routes/search.js
@@ -0,0 +1,45 @@
+const Axios = require('axios').default;
+
+const titles = {
+  'a new hope': 1,
+  'the empire strikes back': 2,
+  'return of the jedi': 3,
+  'phantom menace': 4,
+  'attack of the clones': 5,
+  'revenge of the sith': 6,
+  'WRONG': null
+}
+
+function correctTitle({ params }, res, next) {
+  const { movie } = params;
+  for (let title in titles) {
+    if (title === movie.toLowerCase()) {
+      next();
+      break
+    }
+    if (title === "WRONG") {
+      res.send('FAIL')
+    }
+  }
+}
+
+async function searchSWAPI ({ params }, res, next) {
+  const { movie } = params;
+  const lowerCaseMovie = movie.toLowerCase();
+  let trueOrder = titles[lowerCaseMovie].toString()
+
+  try {
+    const response = await Axios.get(`https://swapi.dev/api/films/${trueOrder}/`)
+    const { data } = response
+    console.log(data)
+    res.send(data)
+  } catch(err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
+}
+
+module.exports = {
+  correctTitle,
+  searchSWAPI
+}
